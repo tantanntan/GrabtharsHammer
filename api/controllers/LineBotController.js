d4c4697900a73a4b3671ca60ce1c2e663347e1b4
TANDAI <tantanntan@gmail.com>
2016-04-11 17:25:36 +0000

    test1

--- api/controllers/LineBotController.js
+++ api/controllers/LineBotController.js
@@ -8,7 +8,11 @@
 module.exports = {
 	callback: function (req, res) {
     	console.log(req.params.all());
-    	console.log(req.param('result').content);
+    	console.log(req.param('result'));
+    	console.log(req.param('result')[0]);
+    	console.log(req.param('result')[0]['content']);
+    	console.log(req['result'][0]['content']);
+    	console.log(['result'][0]['content']['text']);
         var headers = {
             "Content-Type": "application/json; charset=UTF-8",
             "X-Line-ChannelID": sails.config.lineconfig.X_Line_ChannelID,
