551ac9f58432a716782c9c3ba423d2abd24ecf4c
TANDAI <tantanntan@gmail.com>
2016-04-11 16:32:42 +0000

    test1

--- api/controllers/LineBotController.js
+++ api/controllers/LineBotController.js
@@ -6,6 +6,16 @@
  */
 
 module.exports = {
-	
+	callback: function (req, res) {
+    	console.log(req.params);
+	    
+        return res.json(
+        {
+            "X-Line-ChannelID": sails.config.lineconfig.X_Line_ChannelID,
+            "X-Line-ChannelSecret" : sails.config.lineconfig.X_Line_ChannelSecret,
+            "X-Line-Trusted-User-With-ACL": sails.config.lineconfig.X_Line_Trusted_User_With_ACL
+        }    
+        );
+  }
 };
 
