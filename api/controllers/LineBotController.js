bd03b626e1eea1860f586cd290ac9f5dccc6a5eb
TANDAI <tantanntan@gmail.com>
2016-04-13 01:35:15 +0000

    slightly changes

--- api/controllers/LineBotController.js
+++ api/controllers/LineBotController.js
@@ -9,27 +9,24 @@ var request = require('request');
 var headers = {
     "Content-Type": "application/json; charset=UTF-8",
     "X-Line-ChannelID": sails.config.lineconfig.X_Line_ChannelID,
-    "X-Line-ChannelSecret" : sails.config.lineconfig.X_Line_ChannelSecret,
+    "X-Line-ChannelSecret": sails.config.lineconfig.X_Line_ChannelSecret,
     "X-Line-Trusted-User-With-ACL": sails.config.lineconfig.X_Line_Trusted_User_With_ACL
 };
 
 module.exports = {
-	callback: function (req, res) {
-//    	console.log(req.params.all());
-//    	console.log(req.param('result'));
-//    	console.log(req.param('result')[0]);
-//    	console.log(req.param('result')[0]['content']);
-    	var content = req.param('result')[0]['content'];
+    callback: function(req, res) {
+        var content = req.param('result')[0]['content'];
         var to_array = [];
         to_array.push(content['from']);
         var text = content['text'];
-        if( text.length > 20 ) {
+        if (text.length > 20) {
             content['text'] = "話が長いニョロ・・・。";
-        }else{
+        }
+        else {
             content['text'] = 'へえ、「' + text + '」ってこと？がっかりニョロよ';
         }
 
-        var resJson= {
+        var resBody = {
             to: to_array,
             toChannel: 1383378250,
             eventType: "138311608800106203",
@@ -37,35 +34,35 @@ module.exports = {
         };
         var options = {
             url: 'https://trialbot-api.line.me/v1/events',
-            proxy : process.env.FIXIE_URL,
+            proxy: process.env.FIXIE_URL,
             headers: headers,
             json: true,
-            body: resJson
+            body: resBody
         };
 
         res.set(headers);
 
-        request.post(options, function (error, response, body) {
+        request.post(options, function(error, response, body) {
             if (!error && response.statusCode == 200) {
-//                console.log(body);
-                resJson['content']['text']='寂しいニョロか？';
-                responseUser(options,resJson);
-            } else {
-                console.log('error: '+ JSON.stringify(response));
+                //                console.log(body);
+                resBody['content']['text'] = '寂しいニョロか？';
+                sendToUser(options, resBody);
+            }
+            else {
+                console.log('error: ' + JSON.stringify(response));
             }
         });
-        return res.json(
-            200,
-            resJson);
-  }
+        return res.json(200, resBody);
+    }
 };
-  function responseUser(options,content){
-        request.post(options, function (error, response, body) {
-            if (!error && response.statusCode == 200) {
-                console.log(body);
-            } else {
-                console.log('error: '+ JSON.stringify(response));
-            }
-        });     
-  }
 
+function sendToUser(options, content) {
+    request.post(options, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            console.log(body);
+        }
+        else {
+            console.log('error: ' + JSON.stringify(response));
+        }
+    });
+}
---
+++ api/services/sendLineService.js
@@ -0,0 +1,6 @@
+module.exports = {
+    message: function(){
+        
+                
+    }
+};
